d1c050e964ccd01dc2efe33be52e303f2e2c4afc
Don Simon <48922262+ShahriyarHussain@users.noreply.github.com>
2020-12-04 00:03:53 +0600

    Create FilesFragment.java

---
+++ app/src/main/java/com/zulfikar/aaiplayer/FilesFragment.java
@@ -0,0 +1,37 @@
+package com.zulfikar.aaiplayer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import static com.zulfikar.aaiplayer.MainActivity.videoFiles;
+
+public class FilesFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    View view;
+    VideoAdapter videoAdapter;
+
+    public FilesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_files, container, false);
+        recyclerView = view.findViewById(R.id.filesRV);
+        if (videoFiles != null && videoFiles.size() > 0) {
+            videoAdapter = new VideoAdapter(getContext(), videoFiles);
+            recyclerView.setAdapter(videoAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        }
+        return view;
+    }
+}
