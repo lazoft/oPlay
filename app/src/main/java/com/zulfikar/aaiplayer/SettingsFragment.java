ff3cb52fc9df490179bed9f02766c219b33847d3
Don Simon <48922262+ShahriyarHussain@users.noreply.github.com>
2020-12-07 21:00:22 +0600

    Some code addition to themes. Still not functional. Modified file: styles.xml, colors.xml New file: ThemeActivity.java

--- app/src/main/java/com/zulfikar/aaiplayer/SettingsFragment.java
+++ app/src/main/java/com/zulfikar/aaiplayer/SettingsFragment.java
@@ -4,15 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
+//import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SettingsFragment extends Fragment {
 
-    RecyclerView recyclerView;
+    //RecyclerView recyclerView;
+    TextView button;
 //    private static final String PREFS_NAME = "prefs";
 //    private static final String PREF_DARK_THEME = "dark_theme";
     public SettingsFragment() {
@@ -23,9 +25,14 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 //        View view = super.onCreateView(inflater, container, savedInstanceState);
-
-
+        button = view.findViewById(R.id.themeBtn);
+        button.setOnClickListener(view1 -> openThemeActivity());
         return view;
+    }
+
+    public void openThemeActivity() {
+        Intent intent = new Intent(getActivity(), ThemeActivity.class);
+        startActivity(intent);
     }
 
 //    private void toggleTheme(boolean darkTheme) {
--- app/src/main/java/com/zulfikar/aaiplayer/ThemeActivity.java
+++ app/src/main/java/com/zulfikar/aaiplayer/ThemeActivity.java
@@ -3,7 +3,8 @@ package com.zulfikar.aaiplayer;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Switch;
+import android.widget.Button;
+//import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +13,14 @@ public class ThemeActivity extends AppCompatActivity {
     private static final String PREF_DARK_THEME = "dark_theme";
 
     protected void onCreate(Bundle savedInstanceState){
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useDarkTheme = preferences.getBoolean(PREF_DARK_THEME, false);
+        Button toggle = findViewById(R.id.tasniaThemeBtn);
+        if(useDarkTheme) {
+            setTheme(R.style.TasniaTheme);
+        }
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_theme);
         //Switch toggle = (Switch)findViewById()
 
     }
